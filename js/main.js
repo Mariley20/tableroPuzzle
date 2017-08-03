17132794f1de7fd0256c3a2196ded24a6f077055
Mariley20 <mariley2018@gmail.com>
2017-08-03 10:57:28 -0500

    tablero js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
-var btnAgregar = document.getElementById('dibujarTabla');
+var btnAgregar = document.getElementById('dibujaTabla');
 btnAgregar.onclick = function(){
-	var numLados = document.getElementById('n_Lados').value;
-	for(var i = 0; i < parseInt(numLados); i++)	
+	var numLados = parseFloat(document.getElementById('n_Lados').value);
+
+	for(var i = 0; i < parseInt(numLados); i++){
+		var tagTR = document.createElement("tr");
+		for (var j = 0; j < parseInt(numLados); j++) {
+			var tagTD = document.createElement("td");
+			var textContent = document.createTextNode("reina")
+			tagTD.appendChild(textContent);
+			tagTR.appendChild(tagTD);
+		}
+	}
+	document.getElementById('tablero').appendChild(tagTR);
+	tablero1.innerHTML = crearTablero(numLados);
+	
 } 
+function crearTablero(num){
+	html = "<tr>";
+	for(var i = 0; i < num; i++){
+		for (var j = 0; j < num; j++){
+			html += "<td>xx</td>";
+		}
+		html += "</tr><tr>";
+	}
+	return html;
+}
