183559d35946b82f6193d0a8041c7819485b2022
Mariley20 <mariley2018@gmail.com>
2017-08-03 10:27:47 -0500

    tablero de ajedrez html

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var btnAgregar = document.getElementById('dibujarTabla');
+btnAgregar.onclick = function(){
+	var numLados = document.getElementById('n_Lados').value;
+	for(var i = 0; i < parseInt(numLados); i++)	
+} 
